8981501cdc6bbea8830f8377e44ff215bec8d5b5
arek <arek00@yahoo.pl>
2015-10-21 00:46:14 +0200

    Reduced CentroidToClusterConverter with SequenceFileDirValueIterable instance.

--- src/main/java/com/arek00/clusterizer/Clustering/Clusterizers/StreamingKMeans/CentroidToClusterConverter.java
+++ src/main/java/com/arek00/clusterizer/Clustering/Clusterizers/StreamingKMeans/CentroidToClusterConverter.java
@@ -2,32 +2,26 @@ package com.arek00.clusterizer.Clustering.Clusterizers.StreamingKMeans;
 
 
 import com.arek00.clusterizer.Clustering.SequenceFile.SequenceFileWriter;
+import com.google.common.collect.Lists;
 import lombok.NonNull;
-import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
-import org.apache.hadoop.mapred.SequenceFileRecordReader;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.mahout.clustering.Cluster;
-import org.apache.mahout.clustering.canopy.Canopy;
 import org.apache.mahout.clustering.iterator.ClusterWritable;
 import org.apache.mahout.clustering.kmeans.Kluster;
 import org.apache.mahout.clustering.streaming.mapreduce.CentroidWritable;
 import org.apache.mahout.common.Pair;
 import org.apache.mahout.common.distance.DistanceMeasure;
-import org.apache.mahout.common.iterator.sequencefile.SequenceFileIterable;
+import org.apache.mahout.common.iterator.sequencefile.PathFilters;
+import org.apache.mahout.common.iterator.sequencefile.PathType;
+import org.apache.mahout.common.iterator.sequencefile.SequenceFileDirValueIterable;
 import org.apache.mahout.math.Centroid;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -36,20 +30,12 @@ public class CentroidToClusterConverter {
     private static final Logger logger = LogManager.getLogger(CentroidToClusterConverter.class);
 
     private Configuration configuration;
-    private FileSystem fileSystem;
     private DistanceMeasure measure;
 
     public CentroidToClusterConverter(@NonNull Configuration configuration, @NonNull DistanceMeasure measure) {
         this.measure = measure;
         this.configuration = configuration;
-
-        try {
-            this.fileSystem = FileSystem.get(configuration);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
-
 
     /**
      * Convert directory contains created centroids.
@@ -62,14 +48,11 @@ public class CentroidToClusterConverter {
         return saveClusters(outputDirectory, centroids.iterator());
     }
 
-
     private Path saveClusters(Path outputSequenceDirectory, Iterator<Centroid> centroidIterator) throws IOException {
         SequenceFileWriter writer = new SequenceFileWriter(this.configuration);
-
         Path generatedClustersFile = new Path(outputSequenceDirectory, "part-generatedClusters");
 
-        List<Pair<Writable, Writable>> clusters;
-        clusters = new ArrayList<Pair<Writable, Writable>>();
+        List<Pair<Writable, Writable>> clusters = Lists.newArrayList();
 
         centroidIterator.forEachRemaining(centroid -> {
             Cluster cluster = convert(centroid, this.measure);
@@ -85,15 +68,16 @@ public class CentroidToClusterConverter {
     }
 
     private List<Centroid> readCentroids(Path directory) {
-        File directoryFileInstance = new File(directory.toString());
-        List<Centroid> centroids = new ArrayList<Centroid>();
-        Iterator<File> sequenceFiles = FileUtils.iterateFiles(directoryFileInstance, null, false);
+        List<Centroid> centroids = Lists.newArrayList();
 
-        sequenceFiles.forEachRemaining(file -> {
-            getCentroidsFromFile(new Path(file.getAbsolutePath()))
-                    .forEachRemaining(centroid -> {
-                        centroids.add(centroid);
-                    });
+        SequenceFileDirValueIterable<Writable> directoryIterable =
+                new SequenceFileDirValueIterable<Writable>
+                        (directory, PathType.LIST, PathFilters.partFilter(), this.configuration);
+
+        directoryIterable.forEach(sequenceValue -> {
+            if (sequenceValue instanceof CentroidWritable) {
+                centroids.add(((CentroidWritable) sequenceValue).getCentroid());
+            }
         });
 
         return centroids;
@@ -106,27 +90,5 @@ public class CentroidToClusterConverter {
         logger.info("Cluster is converged: " + cluster.isConverged());
 
         return cluster;
-    }
-
-
-    private Iterator<Centroid> getCentroidsFromFile(Path centroidsSequenceFile) {
-        SequenceFileIterable<Writable, Writable> centroidsIterator =
-                new SequenceFileIterable<Writable, Writable>(centroidsSequenceFile, this.configuration);
-
-        List<Centroid> centroidList = new ArrayList<Centroid>();
-
-        try {
-            centroidsIterator.forEach(pair -> {
-                if (pair.getSecond() instanceof CentroidWritable) {
-                    Centroid centroid = ((CentroidWritable) pair.getSecond()).getCentroid();
-                    centroidList.add(centroid);
-                }
-            });
-        } catch(IllegalStateException e) {
-            logger.error(centroidsSequenceFile + " is not a SequenceFile.");
-            return Collections.<Centroid>emptyIterator();
-        }
-
-        return centroidList.iterator();
     }
 }
